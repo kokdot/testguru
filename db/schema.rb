4059e348c247a5a377604d308123e6e36e5effe5
kokdot <kok.74@mail.ru>
2021-02-08 10:47:01 +0500

    lesson_2 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_055716) do
-
-  create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
