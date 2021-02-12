89284378ae7d109e6775e188ca9bd71d049589dd
kokdot74 <kok74@mail.ru>
2021-02-12 22:28:12 +0500

    lesson_5 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :user_tests
   has_many :tests, through: :user_tests
-  has_many :tests
+  has_many :author_tests, class_name: "Test"
 
   def levels(level)
     tests.where(level: level)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 cat = Category.create(title: 'Ruby')
-test = Test.create(title: 'Основы языка Ruby.', level: 1, category_id: cat.id)
+tom = User.create(name: 'Tom', email: 'tom@gmail.com')
+tim = User.create(name: 'Tim', email: 'tim@gmail.com')
+jhon = User.create(name: 'Jhon', email: 'jhon@gmail.com')
+test = Test.create(title: 'Основы языка Ruby.', level: 1, category_id: cat.id, user_id: jhon.id)
 question1 = Question.create(body: 'Чем отличается puts от print:', test_id: test.id)
 question2 = Question.create(body: 'Какой метод позволяет перевести строку в нижний регистр:', test_id: test.id)
 question3 = Question.create(body: "Что выведет этот код: \n some = 25 \n print(“Переменная: ” + some)", test_id: test.id)
-Answer.create([{ body: 'а) Ничем, оба делают одно и тоже', question_id: question1.id, correct: false },
+Answer.create([
+                { body: 'а) Ничем, оба делают одно и тоже', question_id: question1.id, correct: false },
                 { body: 'б) print без пропуска строки, а puts с пропуском', question_id: question1.id, correct: true },
                 { body: 'в) puts позволяет выводить переменные, а print только текст', question_id: question1.id, correct: false },
                 { body: 'а) lowercase()', question_id: question2.id, correct: true },
@@ -18,9 +22,7 @@ Answer.create([{ body: 'а) Ничем, оба делают одно и тоже
                 { body: 'в) downcase() +', question_id: question2.id, correct: false },
                 { body: 'а) Выведет: “Переменная: some”', question_id: question3.id, correct: false },
                 { body: 'б) Выведет: “Переменная: 25”', question_id: question3.id, correct: true },
-                { body: 'в) Будет выведена ошибка', question_id: question3.id, correct: false }])
-tom = User.create(name: 'Tom', email: 'tom@gmail.com')
-tim = User.create(name: 'Tim', email: 'tim@gmail.com')
-User.create(name: 'Jhon', email: 'jhon@gmail.com')
+                { body: 'в) Будет выведена ошибка', question_id: question3.id, correct: false }
+              ])
 UserTest.create( test_id: test.id, user_id: tom.id)
 UserTest.create( test_id: test.id, user_id: tim.id)
