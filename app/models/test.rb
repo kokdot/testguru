7afc8a4271476af3818e667e937a913a59cfb592
kokdot <kok.74@mail.ru>
2021-02-13 15:16:00 +0500

    lesson_5 4

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :user_tests
   has_many :users, through: :user_tests, source: :user
   belongs_to :category
-  belongs_to :author, class_name: "User"
+  belongs_to :author, foreign_key: "user_id" , class_name: "User"
 
   def self.categories(category)
     joins(:category).where("categories.title like ? ", category).order(:title, :desc).pluck(:title)
