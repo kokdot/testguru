0bdd125b142c1ae8c6470d779c57ff2565c26699
kokdot <kok.74@mail.ru>
2021-02-16 19:25:29 +0500

    lesson_6 1

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,10 +4,13 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests, source: :user
   belongs_to :category
   belongs_to :author, foreign_key: "user_id" , class_name: "User"
+  
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true }
+  validates :title, uniqueness: { scope: :level }
 
-  scope :easy, -> { where(level: 0..1).order(created_at: :desc) }
-
-  def self.categories(category)
-    joins(:category).where("categories.title like ? ", category).order(:title, :desc).pluck(:title)
-  end
+  scope :easy_level, -> { where(level: 0..1).order(created_at: :desc) }
+  scope :average_level, -> { where(level: 2..4).order(created_at: :desc) }
+  scope :difficult_level, -> { where(level: 5..Float::INFINITY).order(created_at: :desc) }
+  scope :categories, -> (category) { joins(:category).where("categories.title like ? ", category).  order(:title, :desc).pluck(:title) }
 end
