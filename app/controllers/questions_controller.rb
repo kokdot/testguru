86c7dc70dff9db6266dbb76076dd46ba87eb3b66
kokdot <kok.74@mail.ru>
2021-02-18 10:51:19 +0500

    lesson_7 1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test#, only: :index
+  before_action :find_test, expect: [:destroy]
+  before_action :find_question, only: [:show, :destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -8,26 +9,34 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render plain: "Test: #{@test.title} \nQuestion: \n#{@test.questions.where(id: [params[:id]]).inspect}"
+    render plain: "Test: #{@test.title} \nQuestion: \n#{@question.inspect}"
   end
 
   def new
-
   end
 
   def create
-    question = @test.questions.create(question_params)
-    render plain: question.inspect
+    question = @test.questions.new(question_params)
+    if question.save
+      render plain: "Your question was successfully created: \n#{question.inspect}"
+    else
+      render plain: 'Invalid parameters.'
+    end
   end
 
   def destroy
-    Question.find(params[:id]).destroy
+    @question.destroy
   end
+  
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
