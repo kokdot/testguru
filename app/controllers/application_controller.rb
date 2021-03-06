31496d10a0592ea38002989148a126ad8aac6c28
kokdot <kok.74@mail.ru>
2021-03-06 14:17:39 +0500

    lesson_11 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    if current_user.admin?
-      admin_tests_path
-    else
-      root_path 
-    end
+    current_user.admin? ? admin_tests_path : root_path 
   end
   
   def configure_permitted_parameters
