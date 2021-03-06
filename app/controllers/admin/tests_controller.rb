9e1f4c022cf362d4dba0ef7964f94f1c8358ded9
kokdot <kok.74@mail.ru>
2021-03-06 14:32:13 +0500

    lesson11 5

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -16,7 +16,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = current_user.tests.new(test_params)
+    @test = current_user.author_tests.new(test_params)
 
     if @test.save
       redirect_to admin_test_path(@test)
