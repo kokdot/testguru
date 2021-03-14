f69dc067ddce78eaf93674f890f2efc835fc70be
kokdot <kok.74@mail.ru>
2021-03-14 11:50:36 +0500

    lesson_15 dz 1

---
+++ app/assets/javascripts/utilities/session.js
@@ -0,0 +1,37 @@
+document.addEventListener('turbolinks:load', function() {
+  const caushion_green = document.querySelector('.caushion-green')
+  const caushion_red = document.querySelector('.caushion-red')
+
+  
+  var compare_val = document.querySelector('.compare')
+  var input_val = document.querySelector('.input')
+  
+  input_val.addEventListener('input', updateValue)
+
+  function updateValue(e) {
+    if (compareVal(compare_val, input_val)) {
+      caushion_green.classList.remove('hide')
+      caushion_red.classList.add('hide')
+    } else {
+      caushion_green.classList.add('hide')
+      caushion_red.classList.remove('hide')
+    }
+  }
+
+})
+
+function compareVal(val1, val2) {
+  arr1 = val1.value.split("")
+  arr2 = val2.value.split("")
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1[i] === arr2[i]) { 
+      continue
+    } else { return false }
+  }
+  if (arr2.length > arr1.length) {
+    return false
+  } else {
+    return true
+  }
+
+}
