e079550766424035c954ce5560d316f4fd922fde
kokdot <kok.74@mail.ru>
2021-03-16 16:17:52 +0500

    lesson_16 base

---
+++ app/assets/javascripts/utilities/form_inline.js
@@ -0,0 +1,50 @@
+document.addEventListener('turbolinks:load', function() {
+  // var control = document.querySelectorAll('.form-inline-link')
+  // if (control.length) {
+  //   for (var i = 0; i < control.length; i++) {
+  //     control[i].addEventListener('click', formInLineLinkHandler)
+  //   }
+  // }
+
+  $('.form-inline-link').on('click', formInLineLinkHandler)
+
+  var errors = document.querySelector('.resource-errors')
+  
+  if (errors) {
+    var resourceId = errors.dataset.resourceId
+    formInLineHandler(resourceId)
+  }
+})
+
+function formInLineLinkHandler(event) {
+  event.preventDefault()
+
+  var testId = this.dataset.testId
+  formInLineHandler(testId)
+}
+
+function formInLineHandler(testId) {
+  var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
+  // var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
+  // var formInLine = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+  
+  $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  $formInLine = $('.form-inline[data-test-id="' + testId + '"]')
+
+  $formInLine.toggle()
+  $testTitle.toggle()
+
+  // if (formInLine.classList.contains('hide')) {  
+    if($formInLine.is(':visible')) {
+
+    // }
+    // testTitle.classList.add('hide')
+    // formInLine.classList.remove('hide')
+    link.textContent = 'Cancel'
+  } else{
+    // testTitle.classList.remove('hide')
+    // formInLine.classList.add('hide')
+    link.textContent = 'Edit'
+
+  }
+ }
