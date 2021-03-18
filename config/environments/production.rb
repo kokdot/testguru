34ee6810b51aaf2ae7a02274fa0d745c32cc217c
kokdot <kok.74@mail.ru>
2021-03-18 15:22:46 +0500

    lesson_17 heroku 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     port:                  587,
     user_name:             ENV['SMTP_USERNAME'],
     password:              ENV['SMTP_PASSWORD'],
-    authentication:        'plain'
+    authentication:        'plain',
     enable_starttls_auto:  true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
